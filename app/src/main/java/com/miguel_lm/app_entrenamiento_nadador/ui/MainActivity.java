be92d44c4e0efbf34ed4b5da266ef01d17c5678a
MiguelLm84 <72008078+MiguelLm84@users.noreply.github.com>
2020-11-14 20:51:17 +0100

    creación de slash y pequeños cambios.

--- app/src/main/java/com/miguel_lm/app_entrenamiento_nadador/ui/MainActivity.java
+++ app/src/main/java/com/miguel_lm/app_entrenamiento_nadador/ui/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements SeleccionarEntren
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
